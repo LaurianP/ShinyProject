962f829b62c895811dbb9e4d0408d92414e82658
MariusP <popazyme@gmail.com>
2018-04-27 18:08:15 -0400

    grid to control spacing

--- ui.R
+++ ui.R
@@ -1,27 +1,37 @@
 library(shiny)
 library(shinydashboard)
 
-
+# we can use CSS to create a different style
 shinyUI(dashboardPage(
     dashboardHeader(title = "FDA Adverse Event Data"),
     dashboardSidebar(
-      dateInput(inputId = "reportDateStart", 
-                label = "Minimum FDA Report Date", 
-                value = "2015-01-01", 
-                min = "2015-01-01",
-                max = "2018-04-27"),
-      dateInput(inputId = "reportDateEnd", 
-                label = "Minimum FDA Report Date", 
-                value = "2016-01-01", 
-                min = "2015-01-01",
-                max = "2018-04-27"),
-      selectizeInput(inputId = "outcome",
-                     label = "Patient Outcome",
-                     choices = NULL)
+      sidebarUserPanel("Marius"),
+      sidebarMenu(
+        menuItem("By Sex", tabName="bysex", icon=icon("map"))
+      )
     ),
     dashboardBody(
-      fluidRow(
-        column(6, plotOutput("bysex"))
+      tabItems(
+        tabItem(tabName = "bysex", 
+          fluidRow(
+            column(width=10, offset=1, 
+              dateInput(inputId = "reportDateStart", 
+                        label = "Minimum FDA Report Date", 
+                        value = "2015-01-01", 
+                        min = "2015-01-01",
+                        max = "2018-04-27"),
+              dateInput(inputId = "reportDateEnd", 
+                        label = "Minimum FDA Report Date", 
+                        value = "2016-01-01", 
+                        min = "2015-01-01",
+                        max = "2018-04-27"),
+              selectizeInput(inputId = "outcome",
+                             label = "Patient Outcome",
+                             choices = NULL)
+            )
+          ),
+          fluidRow(column(6, plotOutput("bysex")))
+        )
       )
     )
   )
